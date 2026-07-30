83314815e43c385e8aff1abc94f84664dacd4fa0
Rachelw317 <R1217788708@outlook.com>
2026-07-30 22:10:34 +0800

    Agent set up done.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from agents.resume_agent import agent
 
 file_path = "./tests/测试用简历.docx"
 
-result = agent.invoke({
-    "input": f"请帮我分析这份简历与这份JD的匹配情况，并优化简历，使其更符合JD的要求。我的简历存储在{file_path}中，请帮我分析并优化。"
-})
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": f"我的简历在{file_path}，请帮我解析一下。并返回结构化 JSON。"}]}
+)
 
 print(result)
