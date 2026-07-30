2463facac64b88e4ae44b333d08a9edc87ef4494
Rachelw317 <R1217788708@outlook.com>
2026-07-30 14:22:29 +0800

    Modified main.py

--- main.py
+++ main.py
@@ -1,57 +1,11 @@
-from loaders.file_loader import load_resume
-
-from core.resume_parser import parse_resume
-
-from core.jd_parser import parse_jd
-
-from core.match_analyzer import analyze_match
-
-from core.resume_optimizer import optimize_resume
-
 from tools.markdown_writer import write_markdown
+from tools.resume_tools import load_resume_tool
 
-resume_text = load_resume(
-    "./tests/测试用简历.docx"
-)
-
-
-resume = parse_resume(
-    resume_text
-)
-
-
-
-with open(
-    "./tests/test_jd.txt",
-    encoding="utf-8"
-) as f:
-
-    jd_text=f.read()
-
-
-
-jd = parse_jd(jd_text)
-
-
-
-result = analyze_match(
-    resume,
-    jd
+result = load_resume_tool.invoke(
+    {
+    "file_path": "./tests/测试用简历.docx"
+    }
 )
 
 
 print(result)
-
-optimized_resume = optimize_resume(
-    resume,
-    jd,
-    result
-)
-
-output = write_markdown(
-    optimized_resume,
-    "./outputs/resume_modified.md"
-)
-
-
-print(output)
